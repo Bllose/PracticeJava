0bc77e1d1b503fa69ce4ea4c919d8a676fd36a3d
Bllose <574931094@qq.com>
2023-08-02 20:14:24 +0800

    优化最长字符串长度算法

--- java/longest/longestStringOne.java
+++ java/longest/longestStringOne.java
@@ -1,5 +1,7 @@
 package longest;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -27,7 +29,33 @@ public class longestStringOne {
         processer(inputHolder);
     }
 
+    /*
+    优化内容
+    1、通过取余将顺序遍历转为循环遍历
+    2、过程中我们仅需要将多出来的o队列长度从总长度中剔除，故只需要将o队列长度算出即可。遍历队列时我们直接保存顺序值即可，无需考虑循环队列是否匹配数组的逻辑位置
+     */
     public static void processer(char[] inputHolder) {
+        int LEN = inputHolder.length;
+        Queue<Integer> point = new ArrayDeque<>();
+        point.add(0);
 
+        int max = 0;
+        int current = 0;
+
+        for(int i = 0; i < LEN * 2; i ++) {
+            current ++; // 统计当前队列长度
+            char cur = inputHolder[i%LEN];
+            if(cur == 'o') {
+                point.add(i);
+                point.add(i+1);
+            }
+            if(point.size()>6) {
+                int start = point.poll();
+                current -= (point.poll() - start + 1);
+            }
+            max = current > max?current:max; // 更最长记录做比较，留下最长记录
+        }
+
+        System.out.println(max);
     }
 }
